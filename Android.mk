05847ee55f6f39f467750a3a97960e5f19d45b60
Johan08 <j_susarte@hotmail.cl>
2015-07-31 19:19:57 -0400

    jni

---
+++ Android.mk
@@ -0,0 +1,52 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+
+LOCAL_MODULE := cocos2dcpp_shared
+
+LOCAL_MODULE_FILENAME := libcocos2dcpp
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+                   ../../Classes/AppDelegate.cpp \
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/NewScene.cpp \
+                   ../../Classes/NewScene2.cpp \
+                   ../../Classes/NewScene3.cpp \
+                    ../../Classes/NewScene4.cpp \
+                    ../../Classes/NewScene5.cpp \
+                   ../../Classes/NewScene5.cpp 
+                   
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_ui_static
+
+#LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,.)
+$(call import-module,audio/android)
+$(call import-module,extensions)
+$(call import-module,ui)
+
+# $(call import-module,network)
+# $(call import-module,Box2D)
+# $(call import-module,editor-support/cocosbuilder)
+# $(call import-module,editor-support/spine)
+# $(call import-module,editor-support/cocostudio)
+# $(call import-module,network)
+# $(call import-module,extensions)
